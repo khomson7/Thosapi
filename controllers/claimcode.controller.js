1198267ec6b5c1e582d934284fdde3988039c1e1
Tran <khomson7@gmail.com>
2021-01-13 14:16:38 +0700

    edit

--- controllers/claimcode.controller.js
+++ controllers/claimcode.controller.js
@@ -39,7 +39,7 @@ module.exports = {
             
             return res.json({
                 success: 1,
-                results
+                data: results
             });
         });
     },
--- routes/claimcodes.js
+++ routes/claimcodes.js
@@ -10,7 +10,7 @@ const {
 const { getVisit, getClaim } = require('../controllers/claimcode.controller');
 
 router.get('/visit/:cid', checkToken2 , getVisit);
-router.get('/claim/:vn/:pttype',   getClaim);
+router.get('/claim/:vn/:pttype', checkToken2 ,   getClaim);
 //router.get('/updateserial', checkToken2,  getUpdate);
 
 module.exports = router;
